f95c0f320849c070219b5754c3aaea393e58166c
Daniil Roschoupkin <d.roschoupkin@corp.mail.ru>
2020-11-15 19:59:28 +0300

    Add simple input component

---
+++ src/components/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import Input from './ui/controls/Input';
+
+const App = () => <Input />;
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,13 @@ import React, { FC } from 'react';
 import { baseTheme } from '@styles/theme';
 import { ThemeProvider } from 'react-jss';
 
+import App from './components/App';
 import './styles/global.css';
 
 const Root: FC = () => (
   <ThemeProvider theme={baseTheme}>
     <div>Hello world!</div>
+    <App />
   </ThemeProvider>
 );
 
