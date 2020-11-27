306dc8c1149ded9f822457d16de2044fd1f36887
Daniil Roschoupkin <d.roschoupkin@corp.mail.ru>
2020-11-27 22:11:25 +0300

    Add styles for select

--- src/components/ui/controls/Select/Select.tsx
+++ src/components/ui/controls/Select/Select.tsx
@@ -1,13 +1,52 @@
 import React, { FC } from 'react';
 
-import Dropdown from '@ui/Dropdown';
-import Overlay from '@ui/Overlay';
+import { createUseStyles } from 'react-jss';
 
-const Select: FC = () => (
-  <div>
-    <Dropdown />
-    <Overlay />
-  </div>
-);
+import { Theme } from '@styles/types';
+import Dropdown from '@ui/Dropdown';
+
+import { ControlProps } from '../types';
+
+interface SelectProps extends ControlProps {
+  placeholder?: string;
+}
+
+const useStyles = createUseStyles((theme: Theme) => ({
+  root: {
+    'display': 'block',
+    'width': ({ width }: SelectProps) => width ?? '100%',
+    'height': theme.controls.height,
+    'backgroundColor': theme.colorBg,
+    'borderRadius': theme.controls.borderRadius,
+    'padding': theme.controls.padding,
+    'border': `${theme.controls.sizeBorderWidth}px solid ${theme.colors.border.normal}`,
+    'fontSize': theme.fonts.controls.normal.fontSize,
+    'lineHeight': `${theme.fonts.controls.normal.lineHeight}px`,
+    'transition': 'border-color 0.3s',
+    '&::placeholder': {
+      color: theme.colors.textSecondary.normal,
+    },
+    '&:hover:not($disabled)': {
+      borderColor: theme.colors.border.hover,
+    },
+  },
+  disabled: {
+    backgroundColor: theme.colorBgSecondary,
+    borderColor: theme.colors.border.disabled,
+    cursor: 'default',
+  },
+  focused: {
+    borderColor: theme.colors.border.focus,
+  },
+}));
+
+const Select: FC<SelectProps> = (props) => {
+  const classes = useStyles(props);
+  return (
+    <Dropdown overlay={<div />}>
+      <div className={classes.root} />
+    </Dropdown>
+  );
+};
 
 export default Select;
---
+++ src/components/ui/controls/types.ts
@@ -0,0 +1,5 @@
+export interface ControlProps {
+  width?: number;
+  disabled?: boolean;
+  focused?: boolean;
+}
